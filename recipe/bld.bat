24ee1763a915ad4b7d970b8c9deb29014880ee6e
Matt Chan <thewchan@gmail.com>
2022-08-03 17:49:40 -0400

    Remove reference of coverage3 for real

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,8 +5,8 @@ python -c "import os; print('_'.join(os.environ['PY_VER'].split('.')[0]))" > tem
 set /p PY_VER_MAJ=<temp.txt
 del temp.txt
 
-del %PREFIX%\Scripts\coverage%PY_VER_MAJ%-script.py
-del %PREFIX%\Scripts\coverage%PY_VER_MAJ%.exe
-del %PREFIX%\Scripts\coverage%PY_VER_MAJ%.exe.manifest
+del %PREFIX%\Scripts\coverage-%PY_VER%-script.py
+del %PREFIX%\Scripts\coverage-%PY_VER%.exe
+del %PREFIX%\Scripts\coverage-%PY_VER%.exe.manifest
 
 dir /s "%PREFIX%\Scripts\coverage*"
