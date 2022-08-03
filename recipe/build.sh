2465e1c06ce139081b25695d155c749b2b05bdd8
Matt Chan <thewchan@gmail.com>
2022-08-03 18:48:51 -0400

    Switch back to using env var python

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-which python
-python setup.py install --single-version-externally-managed --record record.txt
+$PYTHON setup.py install --single-version-externally-managed --record record.txt
 
 # Remove versioned entrypoints.
-PY_VER_MAJ=$(python -c "import os; print('_'.join(os.environ['PY_VER'].split('.')[0]))")
+PY_VER_MAJ=$($PYTHON -c "import os; print('_'.join(os.environ['PY_VER'].split('.')[0]))")
 
 rm "${BUILD_PREFIX}/bin/coverage${PY_VER_MAJ}"
 rm "${BUILD_PREFIX}/bin/coverage-${PY_VER}"
 
-ls $BUILD_PREFIX/bin/coverage*
+ls "$BUILD_PREFIX"/bin/coverage*
