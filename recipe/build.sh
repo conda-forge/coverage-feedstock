b472196c4b2f30d4069b8af9e0decb1a024927ab
Matt Chan <thewchan@gmail.com>
2022-08-04 11:57:51 -0400

    Debug ls

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ $PYTHON setup.py install --single-version-externally-managed --record record.txt
 # Remove versioned entrypoints.
 PY_VER_MAJ=$($PYTHON -c "import os; print('_'.join(os.environ['PY_VER'].split('.')[0]))")
 
+ls "${BUILD_PREFIX}"/bin
 rm "${BUILD_PREFIX}/bin/coverage${PY_VER_MAJ}"
 rm "${BUILD_PREFIX}/bin/coverage-${PY_VER}"
 
